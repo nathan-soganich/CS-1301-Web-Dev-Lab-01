2c3c1e5ae3d021e419d2f3370516804dac5afe05
nathan-soganich <nathansoganich@gmail.com>
2025-09-25 19:21:08 -0400

    Add files via upload

---
+++ Lab01/info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpg"
+about_me = "Hello, my name is Nathan Soganich and I am a first year undergraduate Industrial engineering major at Georgia Tech "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/nathan-soganich-a91b21277/"
+my_github_url = "https://github.com/nathan-soganich"
+my_email_address = "nathan@soganich.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Systems Engineering',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2029',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "ENGL 1101", "MATH 1553", "PSYC 1101"], 
+    "names":["Intro to CS", "English Composition 1", "Intro to Linear Algebra", "Intro to Genereal Psychology"], 
+    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
+    "skills":["Fundamentals of programming and Python.", "Rhetorical and information based writing for persuasion", "Fundamentals of linear algebra, mostly row reduction.", "Fundamentals of history of psychology, and our understanding of the mind."],
+    }
+experience_data = {
+    "Summer Swim coach": (["-  Coaching of children 5-16 to improve performance at a competitive level.",
+                                                                          "-  Providing swim instruction to children under 5", ],"Images/coach.jpg"),
+    "Head Lifeguard":(["Managed a team of over a dozen lifguards through summers",
+                                                           "- Providing a safe environment for patrons and staff", "- Creating and modifying schedules as needed"],"Images/lifeguard.jpg"),
+    "Line Cook":(["Prepping, cooking, and cleaning for a pizza resturaunt",
+                      "- Had the highest accuracy rate, and efficiency of any pizza cook"],"Images/mellow.jpg")
+
+}
+
+projects_data = {
+    "Fly Girls Lessons": "Did backgroiund research, needs analysis, and developed a series of lessons teaching elementary and middle school girls how to fly and program drones for the Fly Girls program.",
+}
+
+programming_data = {
+    "Python": 50,
+    "Java": 70,
+    "C++": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C++": "🔍",
+}
+spoken_icons = {
+    "English": "🇬🇧"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent"
+}
+leadership_data = {
+    "Debate Coach": (["- Coaching and judging middle school policy debate"],"Images/debate.jpg"),
+    "Water Polo Captain": (["- Leading team during warmups, games, and practices."],"Images/water_polo.jpeg")
+
+}
+activity_data={
+   "Lambda Chi Alpha" : ["- Member of the Beta Kappa chapter of Lambda Chi Alpha at Georgia Tech", "- Working on social, housing, and philanthropy comittees"],
+    "Hytech Racing Team" : ["- Active member of Georgia Tech's electric formula SAE team","- developing parts for the aero subteam for the car"]
+}
